2eaa300095b28146dd365fa301529b5670f87220
Nathan McCarty <nathan@mccarty.io>
2019-05-05 22:34:18 -0400

    Test index commiting
    Added a test that tests index commiting by dropping the repository and
    recreating it before reading.

--- src/repository.rs
+++ src/repository.rs
@@ -372,4 +372,61 @@ mod tests {
         assert_eq!(data3, out3);
     }
 
+    #[test]
+    fn repository_add_drop_read() {
+        let mut key: [u8; 32] = [0; 32];
+        thread_rng().fill_bytes(&mut key);
+
+        let size = 7 * 10_u64.pow(3);
+        let mut data1 = vec![0_u8; size as usize];
+        thread_rng().fill_bytes(&mut data1);
+        let mut data2 = vec![0_u8; size as usize];
+        thread_rng().fill_bytes(&mut data2);
+        let mut data3 = vec![0_u8; size as usize];
+        thread_rng().fill_bytes(&mut data3);
+
+        let root_dir = tempdir().unwrap();
+        let root_path = root_dir.path().display().to_string();
+        println!("Repo root dir: {}", root_path);
+
+        let backend = Box::new(FileSystem::new_test(&root_path));
+        let key1;
+        let key2;
+        let key3;
+
+        {
+            let mut repo = Repository::new(
+                backend,
+                Compression::ZStd { level: 1 },
+                HMAC::SHA256,
+                Encryption::new_aes256cbc(),
+                &key,
+            );
+
+            println!("Adding Chunks");
+            key1 = repo.write_chunk(&data1).unwrap();
+            key2 = repo.write_chunk(&data2).unwrap();
+            key3 = repo.write_chunk(&data3).unwrap();
+        }
+
+        let backend = Box::new(FileSystem::new_test(&root_path));
+
+        let repo = Repository::new(
+            backend,
+            Compression::ZStd { level: 1 },
+            HMAC::SHA256,
+            Encryption::new_aes256cbc(),
+            &key,
+        );
+
+        println!("Reading Chunks");
+        let out1 = repo.read_chunk(key1).unwrap();
+        let out2 = repo.read_chunk(key2).unwrap();
+        let out3 = repo.read_chunk(key3).unwrap();
+
+        assert_eq!(data1, out1);
+        assert_eq!(data2, out2);
+        assert_eq!(data3, out3);
+    }
+
 }
