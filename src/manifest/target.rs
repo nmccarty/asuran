2f6801b67f8e972a2ed7c447fa87552a187bc8bf
Nathan McCarty <nathan@mccarty.io>
2019-05-12 03:35:07 -0400

    Add BackupTarget and RestoreTarget traits
    Added apis for backup and restore targets.
    These are just barebones functions, and will likely be heavily expaned
    upon in the future. Next additon will most likely be functions for
    reading and writing object metadata

--- src/manifest/target.rs
+++ src/manifest/target.rs
@@ -1,1 +1,22 @@
+use std::io::{Read, Seek, Write};
 
+pub trait BackupTarget {
+    /// Returns a list of object paths in the backend
+    ///
+    /// Paths are plaintext, "/" or "\" delimited strings of form "/path/to/object"
+    /// These paths are treated like file paths, and usually will be filepaths, but
+    /// are not required to represent actual file locations, instead they simply define
+    /// a hierarchy of objects.
+    fn paths(&self) -> &[String];
+
+    /// Takes a path and returns a reader for the path this object represents,
+    /// and an optional call back to call upon put completion,
+    fn read_object(&self, path: &str) -> (Box<dyn Read>, Option<Box<dyn Fn()>>);
+}
+
+pub trait RestoreTarget {
+    type Writer: Write + Seek;
+    /// Takes a path and returns a writer to the object that path represents, as
+    /// well as an optional closure to be called on restore
+    fn write_object(&self, path: &str) -> (Self::Writer, Option<Box<dyn Fn()>>);
+}
