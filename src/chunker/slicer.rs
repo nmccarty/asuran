13c1d890d1b38175552d52c4704026779d7c1bdc
Nathan McCarty <nathan@mccarty.io>
2019-10-17 22:26:02 -0400

    Implement slicer trait

--- src/chunker/slicer.rs
+++ src/chunker/slicer.rs
@@ -2,5 +2,18 @@
 //!
 //! Most typical is content defined slicing, but format specific methods are also quite useful
 
+use crate::repository::UnpackedChunk;
+use std::boxed::Box;
+use std::io::Read;
+
 /// Describes something that can slice objects in to chunks in a defined, repeatable manner
-pub trait Slicer {}
+///
+/// Must store state (including the reader) internally
+pub trait Slicer {
+    /// Inserts a reader into the Slicer
+    ///
+    /// Should clear state and drop previous reader
+    fn add_reader(&mut self, reader: Box<dyn Read>);
+    /// Returns the next slice
+    fn take_slice(&mut self) -> UnpackedChunk;
+}
