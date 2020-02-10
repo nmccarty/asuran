a692f9ca7227f1189f62df942d2bc82e6809419e
Nathan McCarty <nathan@mccarty.io>
2020-02-09 20:59:29 -0500

    Various minor typo fixes
    Fixed some typos and appeased the great clippy

--- asuran-core/src/lib.rs
+++ asuran-core/src/lib.rs
@@ -1,2 +1,13 @@
 #![deny(clippy::pedantic)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::pub_enum_variant_names)]
+#![allow(clippy::if_not_else)]
+#![allow(clippy::similar_names)]
+#![allow(clippy::use_self)]
+#![allow(clippy::shadow_unrelated)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::doc_markdown)]
+#![allow(clippy::missing_errors_doc)]
+// Temporary, will remove
+#![allow(clippy::cast_possible_truncation)]
 pub mod repository;
