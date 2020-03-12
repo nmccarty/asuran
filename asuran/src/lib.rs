14dfaa43d06a8d1fb325f8a6aacd9cdfebde6581
Nathan McCarty <nathan@mccarty.io>
2020-03-12 10:45:23 -0400

    fix: Switch from deyning to warning
    Switched clippy flags from #![deny] to #![warn], to prevent crater builds from mistakenly thinking
    this crate is broken.

--- asuran/src/lib.rs
+++ asuran/src/lib.rs
@@ -3,19 +3,9 @@
 //!
 //! Asuran allows for the storing of multiple rich archives in a single repository,
 //! while providing encryption, compression, and global deduplication.
-#![deny(clippy::all)]
-#![deny(clippy::pedantic)]
-#![allow(clippy::module_name_repetitions)]
-#![allow(clippy::pub_enum_variant_names)]
-#![allow(clippy::if_not_else)]
-#![allow(clippy::similar_names)]
-#![allow(clippy::use_self)]
-#![allow(clippy::shadow_unrelated)]
-#![allow(clippy::must_use_candidate)]
-#![allow(clippy::doc_markdown)]
-#![allow(clippy::missing_errors_doc)]
-// Temporary, will remove
-#![allow(clippy::cast_possible_truncation)]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+#![allow(clippy::must_use)]
 
 use byteorder::{BigEndian, WriteBytesExt};
 use lazy_static::lazy_static;
