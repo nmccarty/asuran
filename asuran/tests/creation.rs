eea0adcf97ff405bfb868409a7adca4e60aef8df
Nathan McCarty <nathan@mccarty.io>
2020-04-19 20:06:17 -0400

    test(asuran): Create integration test for #56
    Create a test that attempts to create a multi-file repository with no encryption.
    
    References #56

---
+++ asuran/tests/creation.rs
@@ -0,0 +1,41 @@
+use asuran::prelude::*;
+use tempfile::tempdir;
+
+async fn create_multifile_repository(encryption: Encryption, compression: Compression, hmac: HMAC) {
+    let directory = tempdir().expect("Unable to open temporary directory.");
+
+    let repo_dir = directory.path();
+
+    let key = Key::random(encryption.key_length());
+    let encrypted_key = EncryptedKey::encrypt_defaults(
+        &key,
+        encryption,
+        "And then there was silence, Just a voice from the other world".as_bytes(),
+    );
+
+    let settings = ChunkSettings {
+        compression,
+        encryption,
+        hmac,
+    };
+
+    let mut mf = MultiFile::open_defaults(repo_dir, Some(settings), &key)
+        .await
+        .expect("Unable to create the multifile repository");
+    mf.write_key(&encrypted_key)
+        .await
+        .expect("Unable to write encrypted key");
+    mf.close().await;
+}
+
+// Attempts to create a multifile repository with no encryption.
+// Specifically addresses gitlab issue #56
+#[tokio::test(threaded_scheduler)]
+async fn create_multifile_noencryption() {
+    create_multifile_repository(
+        Encryption::NoEncryption,
+        Compression::NoCompression,
+        HMAC::Blake3,
+    )
+    .await
+}
