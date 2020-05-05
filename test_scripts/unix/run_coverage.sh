9c643ccd67350fc4571d92430b7f839ba90593f1
Nathan McCarty <nathan@mccarty.io>
2020-05-05 17:10:14 -0400

    test: Add script to run a coverage report

---
+++ test_scripts/unix/run_coverage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Starts the required containers, runs the tests, then destroys the containers
+
+# Start containers
+. $(dirname "$0")/start_containers.sh
+# Prepare the environment
+. $(dirname "$0")/prepare_tests.sh
+# Wait a little bit for environment to be ready
+sleep 3
+env | grep "ASURAN"
+# Run the tests
+cargo tarpaulin "$@"
+TEST_STATUS=$?
+# Stop and destroy the containers
+. $(dirname "$0")/stop_containers.sh
+exit $TEST_STATUS
